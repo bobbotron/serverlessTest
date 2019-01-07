6f9eb258aec00e98aecea0314abec5f46f54307b
Robert Young <bobbotron@gmail.com>
2019-01-07 09:03:10 -0500

    Updated routing for unauthenticated users, updated site name

--- vagrant/notes-app-client/src/App.js
+++ vagrant/notes-app-client/src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
     await Auth.signOut();
 
     this.userHasAuthenticated(false);
-     
+
     this.props.history.push("/login");
   }
 
@@ -54,7 +54,7 @@ class App extends Component {
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">Scratch</Link>
+              <Link to="/">Notable</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
